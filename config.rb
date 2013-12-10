ff3e9b1f85a8f79253c42f38f21cd98e6fbebea3
Brian Graves <git@brianleegraves.com>
2013-12-09 21:19:37 -0600

    project update

--- config.rb
+++ config.rb
@@ -14,18 +14,3 @@ configure :build do
   activate :relative_assets
   set :is_building, true
 end
-
-
-helpers do
-  set :contentAreaCounter, 0
-  def content_area(area)
-    set :contentAreaCounter, contentAreaCounter + 1
-    concat is_building == true ? '<custom type="content" name="contentarea' + contentAreaCounter.to_s + '">' : partial("content/" + area)
-  end
-  def page_data(ns, var)
-    concat is_building == true ? "%%=v(@" + ns + "_" + var + ")=%%" : data.template[ns][var]
-  end
-  def page_link(ns, var)
-    concat is_building == true ? "%%=redirectto(@" + ns + "_" + var + ")=%%" : data.template[ns][var]
-  end
-end
